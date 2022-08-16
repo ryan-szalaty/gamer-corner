9732666bd13592aaa4b4f4fa7c6f2c8f25e9843d
ryan-szalaty <ryan.szalaty@gmail.com>
2022-08-17 08:49:21 +0900

    Fixed problems.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+const path = require("path");
 const bodyParser = require("body-parser");
 const { urlencoded } = require("express");
 const bcrypt = require("bcrypt");
